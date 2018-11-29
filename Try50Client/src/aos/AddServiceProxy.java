df777e45dc5a8010cbbc4e4103cca68481925725
fahad <fahad14@umbc.edu>
2018-11-29 15:26:31 -0500

    Client Code for testing out the webservice

---
+++ Try50Client/src/aos/AddServiceProxy.java
@@ -0,0 +1,54 @@
+package aos;
+
+public class AddServiceProxy implements aos.AddService {
+  private String _endpoint = null;
+  private aos.AddService addService = null;
+  
+  public AddServiceProxy() {
+    _initAddServiceProxy();
+  }
+  
+  public AddServiceProxy(String endpoint) {
+    _endpoint = endpoint;
+    _initAddServiceProxy();
+  }
+  
+  private void _initAddServiceProxy() {
+    try {
+      addService = (new aos.AddServiceServiceLocator()).getAddService();
+      if (addService != null) {
+        if (_endpoint != null)
+          ((javax.xml.rpc.Stub)addService)._setProperty("javax.xml.rpc.service.endpoint.address", _endpoint);
+        else
+          _endpoint = (String)((javax.xml.rpc.Stub)addService)._getProperty("javax.xml.rpc.service.endpoint.address");
+      }
+      
+    }
+    catch (javax.xml.rpc.ServiceException serviceException) {}
+  }
+  
+  public String getEndpoint() {
+    return _endpoint;
+  }
+  
+  public void setEndpoint(String endpoint) {
+    _endpoint = endpoint;
+    if (addService != null)
+      ((javax.xml.rpc.Stub)addService)._setProperty("javax.xml.rpc.service.endpoint.address", _endpoint);
+    
+  }
+  
+  public aos.AddService getAddService() {
+    if (addService == null)
+      _initAddServiceProxy();
+    return addService;
+  }
+  
+  public int add() throws java.rmi.RemoteException{
+    if (addService == null)
+      _initAddServiceProxy();
+    return addService.add();
+  }
+  
+  
+}
---
+++ Try50Client/src/aos/AddServiceService.java
@@ -0,0 +1,16 @@
+/**
+ * AddServiceService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package aos;
+
+public interface AddServiceService extends javax.xml.rpc.Service {
+    public java.lang.String getAddServiceAddress();
+
+    public aos.AddService getAddService() throws javax.xml.rpc.ServiceException;
+
+    public aos.AddService getAddService(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
