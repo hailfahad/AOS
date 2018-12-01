8b661d5090201abccd4df7b854d73c762bc10e6f
fahad <fahad14@umbc.edu>
2018-11-30 21:20:26 -0500

    Added more code and created a queue for keeping a count of ongoing requests

--- Try50/src/aos/AddService.java
+++ Try50/src/aos/AddService.java
@@ -3,21 +3,26 @@ package aos;
 import org.apache.tomcat.jni.Time;
 
 public class AddService implements AddInterface{
-	
-	
-	//Have to add code base for maintaining a queue and adding counters to queue on receipt and then randomly sleep for a while
+	//Should add code for performance logging perspective from WS server perspective
 	
 	public int add() {
+		MyTaskQueue.getInstance().addTask();
+		
+		//Spawn a thread to handle the incoming service.. else it is a blocking call
+		//Leave it as is for now
 		
 		try {
 			Thread.sleep((long) (Math.random()*1000));
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		return 5;
+		MyTaskQueue.getInstance().removeTask();
+		//Should add some code for random exits as well
+		
+		return (int) Math.pow(Math.random(),2);
 	}
 
 	public double myload() {
-		return Math.random();
+		return MyTaskQueue.getInstance().getSize()+Math.random();
 	}
 }
---
+++ Try50/src/aos/MyTaskQueue.java
@@ -0,0 +1,34 @@
+package aos;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MyTaskQueue{
+
+	private Queue<Integer> mytask=null;
+	private static MyTaskQueue myTaskQueueObj=null;
+	
+	private MyTaskQueue() {
+		mytask=new LinkedList<>();
+		
+	}
+	
+	public static MyTaskQueue getInstance() {
+		if(myTaskQueueObj==null) {
+			myTaskQueueObj=new MyTaskQueue();
+		}
+		return myTaskQueueObj;
+	}
+	
+	public void addTask() {
+		mytask.add(1);
+	}
+	
+	public void removeTask() {
+		mytask.remove();
+	}
+	
+	public int getSize() {
+		return mytask.size();
+	}
+}
