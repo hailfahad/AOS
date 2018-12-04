3e5af81e95fdea8bf2100bc2681d3bc44d5629bb
Victor <weishinchen@gmail.com>
2018-12-04 02:40:10 -0500

    LoadBalancer update with url retrieval

--- SuperServer/src/aos/common/LoadBalancer.java
+++ SuperServer/src/aos/common/LoadBalancer.java
@@ -1,8 +1,20 @@
 package aos.common;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
 import aos.listeners.ServerListener;
+
 
 public class LoadBalancer implements Runnable{
 
@@ -15,13 +27,38 @@ public class LoadBalancer implements Runnable{
 		Result=0;
 		
 		String lowestServer = this.findLowestServerLoad();
-		
+		// Get the IP of the lowest Server
+		try {
+			String serverAddress = returnIP(lowestServer);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		// Make a call to the lowest server with the starting value
-		
+		// TODO: how do you make SOAP calls given the URL?
 		// Set the result to what the server returns
 		
 		
 				
+	}
+	private String returnIP(String wsdl) throws ParserConfigurationException, MalformedURLException, SAXException, IOException {
+		String toReturn = "";
+
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		Document xml = db.parse(new URL(wsdl).openStream());
+		toReturn = xml.getElementById("wsdl:port").getAttribute("binding");
+		
+		return toReturn;
 	}
 	
 	public String findLowestServerLoad() {
